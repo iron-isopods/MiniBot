04c6bd356196156ebd8826c3788fa9838f5e599b
abhkri113 <abhiram.krishnakumar@gmail.com>
2026-08-18 00:10:14 -0700

    Signed-off-by: abhkri113 <abhiram.krishnakumar@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @Disabled
-@Autonomous
+@TeleOp
 public class HelloWorld extends OpMode {
     @Override
     public void init() {
