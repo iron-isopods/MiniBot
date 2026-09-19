b6c83f780c6a7650a386c6caa2330c03a3fbcbd2
abhkri7 <abhiram.krishnakumar@gmail.com>
2026-09-19 14:04:18 -0700

    Fix ze errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -264,100 +264,158 @@ import java.util.concurrent.TimeUnit;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
-@TeleOp(name = "Sensor: HuskyLens", group = "Sensor")
+
+
+//@TeleOp(name = "Sensor: HuskyLens", group = "Sensor")
+//public class HelloWorld extends LinearOpMode {
+//
+//    private final int READ_PERIOD = 1;
+//
+//    private HuskyLens huskyLens;
+//
+//    @Override
+//    public void runOpMode()
+//    {
+//        huskyLens = hardwareMap.get(HuskyLens.class, "huskylens");
+//
+//        /*
+//         * This sample rate limits the reads solely to allow a user time to observe
+//         * what is happening on the Driver Station telemetry.  Typical applications
+//         * would not likely rate limit.
+//         */
+//        Deadline rateLimit = new Deadline(READ_PERIOD, TimeUnit.SECONDS);
+//
+//        /*
+//         * Immediately expire so that the first time through we'll do the read.
+//         */
+//        rateLimit.expire();
+//
+//        /*
+//         * Basic check to see if the device is alive and communicating.  This is not
+//         * technically necessary here as the HuskyLens class does this in its
+//         * doInitialization() method which is called when the device is pulled out of
+//         * the hardware map.  However, sometimes it's unclear why a device reports as
+//         * failing on initialization.  In the case of this device, it's because the
+//         * call to knock() failed.
+//         */
+//        if (!huskyLens.knock()) {
+//            telemetry.addData(">>", "Problem communicating with " + huskyLens.getDeviceName());
+//        } else {
+//            telemetry.addData(">>", "Press start to continue");
+//        }
+//
+//        /*
+//         * The device uses the concept of an algorithm to determine what types of
+//         * objects it will look for and/or what mode it is in.  The algorithm may be
+//         * selected using the scroll wheel on the device, or via software as shown in
+//         * the call to selectAlgorithm().
+//         *
+//         * The SDK itself does not assume that the user wants a particular algorithm on
+//         * startup, and hence does not set an algorithm.
+//         *
+//         * Users, should, in general, explicitly choose the algorithm they want to use
+//         * within the OpMode by calling selectAlgorithm() and passing it one of the values
+//         * found in the enumeration HuskyLens.Algorithm.
+//         *
+//         * Other algorithm choices for FTC might be: OBJECT_RECOGNITION, COLOR_RECOGNITION or OBJECT_CLASSIFICATION.
+//         */
+//        huskyLens.selectAlgorithm(HuskyLens.Algorithm.FACE_RECOGNITION);
+//        telemetry.update();
+//        waitForStart();
+//
+//        /*
+//         * Looking for AprilTags per the call to selectAlgorithm() above.  A handy grid
+//         * for testing may be found at https://wiki.dfrobot.com/HUSKYLENS_V1.0_SKU_SEN0305_SEN0336#target_20.
+//         *
+//         * Note again that the device only recognizes the 36h11 family of tags out of the box.
+//         */
+//        while(opModeIsActive()) {
+//            if (!rateLimit.hasExpired()) {
+//                continue;
+//            }
+//            rateLimit.reset();
+//
+//            /*
+//             * All algorithms, except for LINE_TRACKING, return a list of Blocks where a
+//             * Block represents the outline of a recognized object along with its ID number.
+//             * ID numbers allow you to identify what the device saw.  See the HuskyLens documentation
+//             * referenced in the header comment above for more information on IDs and how to
+//             * assign them to objects.
+//             *
+//             * Returns an empty array if no objects are seen.
+//             */
+//            HuskyLens.Block[] blocks = huskyLens.blocks();
+//            telemetry.addData("Block count", blocks.length);
+//            for (int i = 0; i < blocks.length; i++) {
+//                telemetry.addData("Block", blocks[i].toString());
+//                /*
+//                 * Here inside the FOR loop, you could save or evaluate specific info for the currently recognized Bounding Box:
+//                 * - blocks[i].width and blocks[i].height   (size of box, in pixels)
+//                 * - blocks[i].left and blocks[i].top       (edges of box)
+//                 * - blocks[i].x and blocks[i].y            (center location)
+//                 * - blocks[i].id                           (Color ID)
+//                 *
+//                 * These values have Java type int (integer).
+//                 */
+//            }
+//
+//            telemetry.update();
+//        }
+//    }
+//}
+
+//Controller moves servo
+@TeleOp(name = "Concept: Scan Servo", group = "Concept")
+//@Disabled
 public class HelloWorld extends LinearOpMode {
 
-    private final int READ_PERIOD = 1;
+    static final double INCREMENT   = 0.09;     // amount to slew servo each CYCLE_MS cycle
+    static final int    CYCLE_MS    =   20;     // period of each cycle
+    static final double MAX_POS     =  1.0;     // Maximum rotational position
+    static final double MIN_POS     =  0.0;     // Minimum rotational position
 
-    private HuskyLens huskyLens;
+    // Define class members
+    Servo   servo;
+    double  position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
+    double toggle_value = 0;
+    boolean lastx, currx = false;
 
     @Override
-    public void runOpMode()
-    {
-        huskyLens = hardwareMap.get(HuskyLens.class, "huskylens");
+    public void runOpMode() {
 
-        /*
-         * This sample rate limits the reads solely to allow a user time to observe
-         * what is happening on the Driver Station telemetry.  Typical applications
-         * would not likely rate limit.
-         */
-        Deadline rateLimit = new Deadline(READ_PERIOD, TimeUnit.SECONDS);
+        // Connect to servo (Assume Robot Left Hand)
+        // Change the text in quotes to match any servo name on your robot.
+        servo = hardwareMap.get(Servo.class, "left_hand");
 
-        /*
-         * Immediately expire so that the first time through we'll do the read.
-         */
-        rateLimit.expire();
-
-        /*
-         * Basic check to see if the device is alive and communicating.  This is not
-         * technically necessary here as the HuskyLens class does this in its
-         * doInitialization() method which is called when the device is pulled out of
-         * the hardware map.  However, sometimes it's unclear why a device reports as
-         * failing on initialization.  In the case of this device, it's because the
-         * call to knock() failed.
-         */
-        if (!huskyLens.knock()) {
-            telemetry.addData(">>", "Problem communicating with " + huskyLens.getDeviceName());
-        } else {
-            telemetry.addData(">>", "Press start to continue");
-        }
-
-        /*
-         * The device uses the concept of an algorithm to determine what types of
-         * objects it will look for and/or what mode it is in.  The algorithm may be
-         * selected using the scroll wheel on the device, or via software as shown in
-         * the call to selectAlgorithm().
-         *
-         * The SDK itself does not assume that the user wants a particular algorithm on
-         * startup, and hence does not set an algorithm.
-         *
-         * Users, should, in general, explicitly choose the algorithm they want to use
-         * within the OpMode by calling selectAlgorithm() and passing it one of the values
-         * found in the enumeration HuskyLens.Algorithm.
-         *
-         * Other algorithm choices for FTC might be: OBJECT_RECOGNITION, COLOR_RECOGNITION or OBJECT_CLASSIFICATION.
-         */
-        huskyLens.selectAlgorithm(HuskyLens.Algorithm.FACE_RECOGNITION);
+        // Wait for the start button
+        telemetry.addData(">", "Press Start to scan Servo." );
         telemetry.update();
         waitForStart();
 
-        /*
-         * Looking for AprilTags per the call to selectAlgorithm() above.  A handy grid
-         * for testing may be found at https://wiki.dfrobot.com/HUSKYLENS_V1.0_SKU_SEN0305_SEN0336#target_20.
-         *
-         * Note again that the device only recognizes the 36h11 family of tags out of the box.
-         */
-        while(opModeIsActive()) {
-            if (!rateLimit.hasExpired()) {
-                continue;
-            }
-            rateLimit.reset();
 
-            /*
-             * All algorithms, except for LINE_TRACKING, return a list of Blocks where a
-             * Block represents the outline of a recognized object along with its ID number.
-             * ID numbers allow you to identify what the device saw.  See the HuskyLens documentation
-             * referenced in the header comment above for more information on IDs and how to
-             * assign them to objects.
-             *
-             * Returns an empty array if no objects are seen.
-             */
-            HuskyLens.Block[] blocks = huskyLens.blocks();
-            telemetry.addData("Block count", blocks.length);
-            for (int i = 0; i < blocks.length; i++) {
-                telemetry.addData("Block", blocks[i].toString());
-                /*
-                 * Here inside the FOR loop, you could save or evaluate specific info for the currently recognized Bounding Box:
-                 * - blocks[i].width and blocks[i].height   (size of box, in pixels)
-                 * - blocks[i].left and blocks[i].top       (edges of box)
-                 * - blocks[i].x and blocks[i].y            (center location)
-                 * - blocks[i].id                           (Color ID)
-                 *
-                 * These values have Java type int (integer).
-                 */
-            }
+        // Scan servo till stop pressed.
+        while(opModeIsActive()){
 
+            // slew the servo, according to the rampUp (direction) variable.
+           position = toggle_value;
+           currx = gamepad1.x;
+           if (currx && !lastx){
+               toggle_value = 1 - toggle_value;
+           }
+            lastx = currx;
+            // Display the current value
+            telemetry.addData("Servo Position", "%5.2f", toggle_value);
+            telemetry.addData(">", "Press Stop to end test." );
             telemetry.update();
+
+            // Set the servo to the new position and pause;
+            servo.setPosition(position);
+            sleep(CYCLE_MS);
+            idle();
         }
+
+        // Signal done;
+        telemetry.addData(">", "Done");
+        telemetry.update();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fixConnection.sh
@@ -0,0 +1,3 @@
+adb kill-server
+adb connect 192.168.43.1
+ping 192.168.43.1
